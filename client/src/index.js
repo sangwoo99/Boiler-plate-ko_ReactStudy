92fde19748b06df1fcf8c0b3f3dc0cae7e0cf7b0
sangwoo <tkddnbanana@gmail.com>
2021-12-23 01:22:50 +0900

    redux기본

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import 'antd/dist/antd.css';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducers';
+
+// 일반적으로 redux는 object만 받아서 promise와 function도 받기위해 redux-promise, redux-thunk를 이용
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    <Provider
+      store={createStoreWithMiddleware(Reducer, 
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+    >
+      <App />
+    </Provider>
+  ,
   document.getElementById('root')
 );
 
--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 5000
+
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
-const { auth } = require('./server/middleware/auth')
+const { auth } = require('./middleware/auth')
 const { User } = require('./models/User');
 
 //application/x-www-form
@@ -22,6 +22,11 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => {
   res.send('Hello World! 안녕하세요')
+})
+
+
+app.get('/api/hello', (req,res) => {
+  res.send("안녕하세요~~")
 })
 
 app.post('/api/users/register', (req,res) => {
@@ -91,6 +96,8 @@ app.post('/api/users/login', (req, res) => {
   })
 
 })
+
+const port = 5000;
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
